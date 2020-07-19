49dc4af502605627c0ff52d26f89c98828599535
ankur6186 <ankursingh6186@gmail.com>
2020-07-19 18:59:11 +0530

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,76 @@
+pragma solidity ^0.5.8;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    function strConcat(string memory _a, string memory _b) internal returns (string memory){
+        bytes memory _ba = bytes(_a);
+        bytes memory _bb = bytes(_b);
+        string memory ab = new string(_ba.length + _bb.length);
+        bytes memory bab = bytes(ab);
+        uint k = 0;
+        for (uint i = 0; i < _ba.length; i++) bab[k++] = _ba[i];
+        for (uint i = 0; i < _bb.length; i++) bab[k++] = _bb[i];
+        return string(bab);
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+
+    constructor () public {
+        for(uint i=1;i<5;i++){
+            string memory candidateName = uint2str(i);
+            candidateName = strConcat("Candidate ",candidateName);
+            addCandidate(candidateName);
+        }
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint _candidateId) public{
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // update candidate vote count
+        candidates[_candidateId].voteCount++;
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+    }
+
+}
